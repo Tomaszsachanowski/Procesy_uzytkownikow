8a0320dbaf549f36bb8cd0129f64861f72eb5a0b
Tomaszsachanowski <tomaszsachanowski@gmail.com>
2019-10-29 13:46:54 +0100

    add func to retrun list of process for user

--- main.pl
+++ main.pl
@@ -13,3 +13,18 @@ sub list_all_user{
     return @list_users;# return all list of users
 }
 
+sub process_user{
+    # `user name` is arrgument
+    # creat list of all process for user
+    my @list_process_user = ();# empty list
+    my ($user_name) = @_;# user_name = arrgument
+    # run command ps -o user,pid,comm --user <user_name> 
+    my $command = "ps -o user,pid,comm --user ";
+    $command = $command . $user_name; # add user name to command
+    
+    foreach my $process (`$command`) {
+    push @list_process_user, $process;# add process to list
+    }
+
+    return @list_process_user;
+}
